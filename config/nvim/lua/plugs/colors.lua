1ecaeabb17e8a0953a3c5fc516769651fbee97a6
Nicola Leonardi <nicolaleonardi@outlook.com>
2025-04-22 14:48:48 +0200

    feat(dashboard): added alpha and customized dashboard
    just one dash to rule them all

--- config/nvim/lua/plugs/colors.lua
+++ config/nvim/lua/plugs/colors.lua
@@ -12,31 +12,8 @@ return {
 		integrations = {
 			gitsigns = true,
 			fzf = true,
+			alpha = true,
 		},
-		--integrations = {
-		--	fzf = true,
-		--	treesitter = true,
-		--	indent_blankline = {
-		--		enabled = true,
-		--		colored_indent_levels = false,
-		--	},
-		--	native_lsp = {
-		--		enabled = true,
-		--		virtual_text = {
-		--			errors = { "italic" },
-		--			hints = { "italic" },
-		--			warnings = { "italic" },
-		--			information = { "italic" },
-		--		},
-		--		underlines = {
-		--			errors = { "underline" },
-		--			hints = { "underline" },
-		--			warnings = { "underline" },
-		--			information = { "underline" },
-		--		},
-		--	},
-		--	cmp = true,
-		--},
 	},
 	config = function(_, opts)
 		require("catppuccin").setup(opts)
